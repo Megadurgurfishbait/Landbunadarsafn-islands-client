ab63f4101e3705e5e842b0990adc1d7a2b5f9068
Megadurgurfishbait <gummehh@gmail.com>
2018-10-30 17:29:55 +0000

    Change of backend

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // Hold application secrets and config.
-/*
+
 module.exports = {
       slod: 'http://178.128.162.183:8000'
 };
-*/
+/*
 module.exports = {
       slod: 'http://localhost:8000'
 };
-
+*/
 
 
